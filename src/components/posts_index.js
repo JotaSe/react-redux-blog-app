f6c35cc1e767f76b9eb4e1502ce6405a8a376001
jotase <jota.segovia@gmail.com>
2016-11-20 14:03:24 -0300

    6-82

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -4,7 +4,7 @@ import { fetchPosts } from '../actions/index';
 
 class PostIndex extends Component {
   componentWillMount() {
-    this.props.fetchPosts()
+    this.props.fetchPosts();
   }
 
   render() {
